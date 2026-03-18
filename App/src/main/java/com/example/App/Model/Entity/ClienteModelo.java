c9f5ae6bf30325b53c276b0fd382ab73f6c20225
kaiquehsp <kaiquehsp5@gmail.com>
2026-03-18 11:44:19 -0300

    feat(US07-01): adiciona modelo de dados do cliente
    essa tabela tem a finalidade de armazenar os dados do cliente.

---
+++ App/src/main/java/com/example/App/Model/Entity/ClienteModelo.java
@@ -0,0 +1,38 @@
+package com.example.App.Model.Entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clientes")
+public class ClienteModelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeEmpresa;
+
+    @Column(nullable = false)
+    private String nomeResponsavel;
+
+    @Column(nullable = false, unique = true, length = 200)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 14)
+    private String cnpj;
+
+    @Column(nullable = false)
+    private LocalDate dataCadastro;
+
+    @Column(nullable = false)
+    private boolean ativo = true;
+}

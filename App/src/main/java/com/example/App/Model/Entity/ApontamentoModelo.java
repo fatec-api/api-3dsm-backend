25f9bd773e6a421526623a3126bc40dab675a380
kaiquehsp <kaiquehsp5@gmail.com>
2026-03-18 11:42:24 -0300

    feat(US06-01): adiciona modelo de dados do apontamento de horas
    essa tabela tem a finalidade de armazenar os registros de apontamento de horas.

---
+++ App/src/main/java/com/example/App/Model/Entity/ApontamentoModelo.java
@@ -0,0 +1,47 @@
+package com.example.App.Model.Entity;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "apontamentos_horas")
+public class ApontamentoModelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_item", nullable = false)
+    private ItemModelo item;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private UsuarioModelo profissional;
+
+    @Column(nullable = false)
+    private LocalDateTime dataApontamento;
+
+    @Column(nullable = false)
+    private LocalDateTime horaInicio;
+
+    @Column(nullable = false)
+    private LocalDateTime horaFim;
+
+    private LocalTime pausaInicio;
+    private LocalTime pausaFim;
+
+    @Column(length = 300)
+    private String observacao;
+
+    @Column(nullable = false)
+    private Double horasLiquidas;
+}

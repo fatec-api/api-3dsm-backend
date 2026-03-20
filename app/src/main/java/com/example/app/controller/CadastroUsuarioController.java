ecd89ecc59b6a0f911a911dc289e06c6a3aaeeaa
Lucas Inacio de Carvalho <lucasinacio8000@gmail.com>
2026-03-20 20:34:00 -0300

    Feat(US01-07)Implementando controle de cadastro de usuários

---
+++ app/src/main/java/com/example/app/controller/CadastroUsuarioController.java
@@ -0,0 +1,29 @@
+package com.example.app.controller;
+
+import com.example.app.dto.request.UsuarioRequestdto;
+import com.example.app.dto.response.UsuarioResponsedto;
+import com.example.app.model.entity.UsuarioModel;
+import com.example.app.service.CadastroUsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CadastroUsuarioController {
+
+    @Autowired
+    CadastroUsuarioService cadastroUsuarioService;
+
+    @PostMapping("/cadastrar/usuario")
+    public ResponseEntity<UsuarioResponsedto> cadastrarUsuario(@RequestBody @Valid UsuarioRequestdto usuarioRequestdto){
+        UsuarioModel usuarioModel = cadastroUsuarioService.cadastrarUsuario(usuarioRequestdto);
+
+        UsuarioResponsedto resposta = new UsuarioResponsedto(usuarioModel.getNomeUsuario(), usuarioModel.getEmail(), usuarioModel.getSenha(), usuarioModel.getValorHora(), usuarioModel.getNivelExperiencia(), usuarioModel.getCargo(), usuarioModel.isAtivo(), usuarioModel.getCriado_em());
+        return new ResponseEntity<>(resposta, HttpStatus.CREATED);
+    }
+
+}

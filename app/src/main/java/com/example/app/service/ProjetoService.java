f4fda609e23959110a4b365153f5cc9a6ebfc924
Kodatoo <gkodatofaria@gmail.com>
2026-03-21 12:17:47 -0300

    feat(US02-02): Adicionados DTOs de request e response e finalizada a lógica da camada de serviço

--- app/src/main/java/com/example/app/service/ProjetoService.java
+++ app/src/main/java/com/example/app/service/ProjetoService.java
@@ -3,40 +3,49 @@ package com.example.app.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.app.model.entity.ClienteModel;
 import com.example.app.model.entity.ProjetoModel;
 import com.example.app.model.entity.UsuarioModel;
+import com.example.app.repository.ClienteRepository;
 import com.example.app.repository.ProjetoRepository;
 import com.example.app.repository.UsuarioRepository;
 
 @Service
 public class ProjetoService {
+
     @Autowired
     private ProjetoRepository projetoRepository;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public ProjetoModel criarProjeto(ProjetoModel projeto){
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-        if (projeto.getDataFim().isBefore(projeto.getDataInicio())){
-            throw new  IllegalArgumentException("Data fim não pode ser anterior à data de início");
+    public ProjetoModel criarProjeto(ProjetoModel projeto) {
+
+        if (projeto.getDataFim().isBefore(projeto.getDataInicio())) {
+            throw new IllegalArgumentException("Data fim não pode ser anterior à data de início");
         }
 
-        if(projeto.getGestor() == null || projeto.getGestor().getId() == null){
-            throw new  IllegalArgumentException("Gestor não encontrado");
+        if (projeto.getGestor() == null || projeto.getGestor().getId() == null) {
+            throw new IllegalArgumentException("Gestor é obrigatório");
         }
 
-        // verifica se já existe o gestor
         UsuarioModel gestor = usuarioRepository.findById(projeto.getGestor().getId())
-            .orElseThrow(() -> new IllegalArgumentException("Gestor não encontrado"));
+                .orElseThrow(() -> new IllegalArgumentException("Gestor não encontrado"));
 
-    
         projeto.setGestor(gestor);
 
-        //profissional opcional
+        if (projeto.getCliente() != null && projeto.getCliente().getId() != null) {
+            ClienteModel cliente = clienteRepository.findById(projeto.getCliente().getId())
+                    .orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado"));
 
-        
-    
+            projeto.setCliente(cliente);
+        }
+
+        projeto.setAtivo(true);
+
+        return projetoRepository.save(projeto);
     }
-
 }

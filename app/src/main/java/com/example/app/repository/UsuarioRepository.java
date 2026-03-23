f969f3a73d4b926aa6e171a0bdeeca041ebddb9b
Lukitta013 <lucasinacio8000@gmail.com>
2026-03-23 16:09:10 -0300

    Chore(US01-07): Modificando para o repositório receber UUID

--- app/src/main/java/com/example/app/repository/UsuarioRepository.java
+++ app/src/main/java/com/example/app/repository/UsuarioRepository.java
@@ -3,5 +3,7 @@ package com.example.app.repository;
 import com.example.app.model.entity.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
 }

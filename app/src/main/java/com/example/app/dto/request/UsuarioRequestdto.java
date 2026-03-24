293d748724b83dcbcda4667b399b24eb6040417c
Lukitta013 <lucasinacio8000@gmail.com>
2026-03-24 15:29:49 -0300

    Fix(US01-07):COrrigindo erro do request e aprimnorando sua segurança.

--- app/src/main/java/com/example/app/dto/request/UsuarioRequestdto.java
+++ app/src/main/java/com/example/app/dto/request/UsuarioRequestdto.java
@@ -1,8 +1,7 @@
 package com.example.app.dto.request;
 
 import com.example.app.model.entity.UsuarioModel;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -10,19 +9,26 @@ import java.sql.Timestamp;
 
 @Data
 public class UsuarioRequestdto {
-    @NotBlank
+    @NotBlank(message = "O nome não pode estar vazio.")
     private String nomeUsuario;
-    @NotBlank
+    @NotBlank(message = "O email não pode estar vazio.")
+    @Email(message = "Email Inválido.")
     private String email;
-    @NotBlank
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
+    @Pattern(
+            regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$",
+            message = "Senha deve conter maiúscula, minúscula, número e caractere especial"
+    )
     private String senha;
-    @NotNull
+
+    @NotNull(message = "Valor hora é obrigatório.")
+    @DecimalMin(value = "0.01", message = "Valor hora mairo que zero.")
     private BigDecimal valorHora;
-    @NotNull
-    private UsuarioModel.NivelExperiencia nivelExperiencia;
+
     @NotNull
     private UsuarioModel.Cargo cargo;
-
+    @NotNull
     private boolean ativo;
     @NotNull
     private Timestamp criado_em;

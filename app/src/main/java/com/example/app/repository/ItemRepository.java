136ced95cea719bf1ea0b650a71149b576680360
Laís Zanardi Inocêncio <89917698+lais-zanardi@users.noreply.github.com>
2026-03-24 11:29:15 -0300

    feat(US01-02): implementação de autenticação e autorização com Keycloak (OAuth2 + OIDC)
    - configura a integração do keycloak com o springboot
    - adiciona login OAuth2 com fluxo de código de autorização
    - implementa servidor de recursos jwt para proteção da api
    - mepeia  roles do keycloak para autoridades do spring security
    - habilita controle de acesso baseado em funções com @PreAuthorize
    - adiciona endpoints de teste para roles de Admin e User

--- app/src/main/java/com/example/app/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.app.model.entity.ItemModel;
-
-public interface ItemRepository extends JpaRepository<ItemModel, Long>{
-    
-}

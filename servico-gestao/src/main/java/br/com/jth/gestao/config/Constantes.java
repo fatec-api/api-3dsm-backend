2ce69a911498f295e512d8d2d6708288cd6d1493
Laís <zanardilais@gmail.com>
2026-04-16 07:21:30 -0300

    feat(US07-06): adiciona estrutura de pastas básicas, pom e dockerfile de cada serviço

---
+++ servico-gestao/src/main/java/br/com/jth/gestao/config/Constantes.java
@@ -0,0 +1,9 @@
+package com.jth.gestao.config
+
+public class Constantes {
+    // Roles
+    public static final String USER = "hasAuthority('USER')";
+    public static final String ADMIN = "hasAuthority('ADMIN')";
+    public static final String ADMIN_OR_USER = "hasAuthority('ADMIN') or #userId == authentication.name";
+    }
+

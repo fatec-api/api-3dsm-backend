ea9f4b88d751d6aa602bce47d82baff9dfb1db05
mariaflbss <mariaf.laboissiere@gmail.com>
2026-04-24 18:24:06 -0300

    feat(US07-11): Adicionando campo status e justificativa ao apontamento no response DTO

--- app/src/main/java/com/example/app/dto/response/ApontamentoResponseDTO.java
+++ app/src/main/java/com/example/app/dto/response/ApontamentoResponseDTO.java
@@ -7,25 +7,36 @@ import java.util.UUID;
 
 public record ApontamentoResponseDTO(
         Long id,
+
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime criadoEm,
+
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime atualizadoEm,
+
         Long itemId,
         String itemDescricao,
         UUID usuarioId,
+
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime dataApontamento,
+
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime horaInicio,
+
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime horaFim,
+
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime pausaInicio,
+
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime pausaFim,
+
         Double horasLiquidas,
-        String observacao
+        String observacao,
+        String status,
+        String justificativa
 
 ) {
 }

116fa4ebcda0de76b856da2297a2b625c2ac15a1
Templasan <joaovictordosreissantos439@gmail.com>
2026-05-01 18:57:14 -0300

    fix(US07-13): Adequação as permissões esperadas no projeto

--- servico-gateway/src/main/java/br/com/jth/gateway/config/SecurityConfig.java
+++ servico-gateway/src/main/java/br/com/jth/gateway/config/SecurityConfig.java
@@ -1,79 +1,155 @@
 package br.com.jth.gateway.config;
 
+
+
 import br.com.jth.gateway.utils.KeycloakRoleConverter;
+
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.core.convert.converter.Converter;
+
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+
 import org.springframework.security.oauth2.jwt.Jwt;
+
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
 import org.springframework.web.cors.CorsConfiguration;
+
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
+
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 import reactor.core.publisher.Mono;
 
+
+
 import java.util.Arrays;
+
 import java.util.List;
 
+
+
 @Configuration
+
 @EnableWebFluxSecurity
+
 public class SecurityConfig {
 
+
+
     @Bean
+
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
         return http
+
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+
                 .authorizeExchange(exchanges -> exchanges
+
                         .pathMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
+
                         .pathMatchers("/actuator/**").permitAll()
+
                         
+
                         // --- EXCEÇÃO ---
+
                         .pathMatchers(org.springframework.http.HttpMethod.GET, "/apontamento/apontamento/usuario/**").authenticated()
+
                         .pathMatchers(org.springframework.http.HttpMethod.GET, "/gestao/itens/usuario/**").authenticated()
+
                         .pathMatchers(org.springframework.http.HttpMethod.PUT, "/gestao/usuarios/atualizar/**").authenticated()
+
                         
+
                         .pathMatchers(org.springframework.http.HttpMethod.POST, "/apontamento/apontamentos/").authenticated()
+
                         .pathMatchers(org.springframework.http.HttpMethod.PATCH, "/apontamento/apontamentos/**/status").hasAnyRole("GESTOR") 
+
                         .pathMatchers(org.springframework.http.HttpMethod.PATCH, "/apontamento/apontamentos/**").authenticated()
 
+
+
                         // --- REGRA GERAL ---
+
                         .pathMatchers("/gestao/**").hasAnyRole("GESTOR") 
+
                         
+
                         .pathMatchers("/apontamento/**").hasAnyRole("GESTOR") 
 
+
+
                         .pathMatchers("/auditoria/**").hasAnyRole("GESTOR") 
+
                         
+
                         .anyExchange().authenticated()
+
                 )
+
                 .oauth2ResourceServer(oauth2 -> oauth2
+
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+
                 )
+
                 .build();
+
     }
 
+
+
     @Bean
+
     public Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
         converter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
+
+
         return new ReactiveJwtAuthenticationConverterAdapter(converter);
+
     }
+
+
 
     @Bean
+
     public CorsConfigurationSource corsConfigurationSource() {
+
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowedOriginPatterns(List.of("*"));
+
         config.setAllowedMethods(List.of("*"));
+
         config.setAllowedHeaders(List.of("*"));
+
         config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
-}
 
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+
+    }
+
+}

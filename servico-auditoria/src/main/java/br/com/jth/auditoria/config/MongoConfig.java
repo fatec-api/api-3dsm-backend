ceecb2a61459919e3c7da1beb701dab157da45bb
Gustavo Rosa <gustavorib.rosa@gmail.com>
2026-05-02 16:49:01 -0300

    refactor(US07-13): Criando a tabela correta no mongodb para salvar a collection logs_auditoria

--- servico-auditoria/src/main/java/br/com/jth/auditoria/config/MongoConfig.java
+++ servico-auditoria/src/main/java/br/com/jth/auditoria/config/MongoConfig.java
@@ -7,17 +7,27 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoConfig {
+
+    private static final String DATABASE = "auditoria";
 
     @Bean
     public MongoClient mongoClient() {
         ConnectionString connectionString = new ConnectionString(
                 "mongodb://mongouser:mongopass@mongodb:27017/auditoria?authSource=admin");
+
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
+
         return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, DATABASE);
     }
 }

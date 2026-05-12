e92107ae84b2408099643dbeac32cedb79615faf
mariaflbss <mariaf.laboissiere@gmail.com>
2026-05-12 17:31:47 -0300

    feat(US15-05): Adicionando endpoint para listagem e busca  de clientes

--- servico-gestao/src/main/java/br/com/jth/servico_gestao/controller/ClienteController.java
+++ servico-gestao/src/main/java/br/com/jth/servico_gestao/controller/ClienteController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/clientes")
 @RequiredArgsConstructor
@@ -22,6 +24,23 @@ public class ClienteController {
 
         ClienteResponseDTO response = clienteService.cadastrar(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteResponseDTO>> listarTodos() {
+        return ResponseEntity.ok(clienteService.listarTodos());
+    }
+ 
+    @GetMapping("/ativos")
+    public ResponseEntity<List<ClienteResponseDTO>> listarAtivos() {
+        return ResponseEntity.ok(clienteService.listarAtivos());
+    }
+ 
+    @GetMapping("/busca")
+    public ResponseEntity<List<ClienteResponseDTO>> buscar(
+            @RequestParam(required = false) String q) {
+ 
+        return ResponseEntity.ok(clienteService.buscar(q));
     }
 
 }

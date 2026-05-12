152c142c24a5931b97ef5f2b507e94cb905cbb3b
mariaflbss <mariaf.laboissiere@gmail.com>
2026-05-12 17:41:33 -0300

    feat(US15-08): Adicionando validações de busca por clientes ativos no repository

--- servico-gestao/src/main/java/br/com/jth/servico_gestao/repository/ClienteRepository.java
+++ servico-gestao/src/main/java/br/com/jth/servico_gestao/repository/ClienteRepository.java
@@ -4,10 +4,19 @@ import br.com.jth.servico_gestao.model.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
 
     // validações de unicidade do email e cnpj do cliente
     Optional<ClienteModel> findByEmail(String email);
     Optional<ClienteModel> findByCnpj(String cnpj);
+
+    List<ClienteModel> findByAtivoTrue();
+
+    // busca por nome da empresa e cnpj ativos
+    List<ClienteModel> findByAtivoTrueAndNomeEmpresaContainingIgnoreCaseOrAtivoTrueAndCnpjContaining(
+            String nomeEmpresa,
+            String cnpj
+    );
 }

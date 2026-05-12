15388a893a0b6cab1eddfd41d4ec90bb43e1db7e
mariaflbss <mariaf.laboissiere@gmail.com>
2026-05-12 17:38:26 -0300

    feat(US15-06): Adicionanando validações de unicidade do email e cnpj do cliente no repository

--- servico-gestao/src/main/java/br/com/jth/servico_gestao/repository/ClienteRepository.java
+++ servico-gestao/src/main/java/br/com/jth/servico_gestao/repository/ClienteRepository.java
@@ -3,5 +3,11 @@ package br.com.jth.servico_gestao.repository;
 import br.com.jth.servico_gestao.model.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+
+    // validações de unicidade do email e cnpj do cliente
+    Optional<ClienteModel> findByEmail(String email);
+    Optional<ClienteModel> findByCnpj(String cnpj);
 }

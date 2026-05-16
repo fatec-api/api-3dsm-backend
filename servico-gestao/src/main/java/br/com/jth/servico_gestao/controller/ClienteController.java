f94532c424e6d3cd28e3265cbfb6227aa4ebcf35
Kaique Henrique <kaiquehsp5@gmail.com>
2026-05-16 10:17:36 -0300

    refactor(US15-10): correcao rota cliente

--- servico-gestao/src/main/java/br/com/jth/servico_gestao/controller/ClienteController.java
+++ servico-gestao/src/main/java/br/com/jth/servico_gestao/controller/ClienteController.java
@@ -23,7 +23,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/gestao/clientes")
+@RequestMapping("/clientes")
 @RequiredArgsConstructor
 public class ClienteController {
 

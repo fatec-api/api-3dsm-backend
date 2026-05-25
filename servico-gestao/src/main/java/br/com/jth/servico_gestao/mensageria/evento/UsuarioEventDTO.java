4069c0ce31cf2846b8fed0aadf1bfab0f6c0938c
Lukitta013 <lucasinacio8000@gmail.com>
2026-05-25 11:42:01 -0300

    Chore(US11-03):O evento agora aceita mais de um cargo.

--- servico-gestao/src/main/java/br/com/jth/servico_gestao/mensageria/evento/UsuarioEventDTO.java
+++ servico-gestao/src/main/java/br/com/jth/servico_gestao/mensageria/evento/UsuarioEventDTO.java
@@ -2,13 +2,14 @@ package br.com.jth.servico_gestao.mensageria.evento;
 
 import br.com.jth.servico_gestao.enums.usuario.Cargo;
 import java.math.BigDecimal;
+import java.util.Set;
 import java.util.UUID;
 
 public record UsuarioEventDTO(
         UUID id,
         String nomeUsuario,
         String email,
-        Cargo cargo,
+        Set<Cargo> cargos,
         BigDecimal valorHora,
         boolean ativo
 ) {}

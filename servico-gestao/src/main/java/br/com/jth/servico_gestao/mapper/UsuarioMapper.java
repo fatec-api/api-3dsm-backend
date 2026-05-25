d40e9e3aedfd0d072f9e9b42fd11e417034425ca
Lukitta013 <lucasinacio8000@gmail.com>
2026-05-25 11:39:15 -0300

    Chore(US11-03): O cargo agora virá do keycloak.

--- servico-gestao/src/main/java/br/com/jth/servico_gestao/mapper/UsuarioMapper.java
+++ servico-gestao/src/main/java/br/com/jth/servico_gestao/mapper/UsuarioMapper.java
@@ -9,10 +9,11 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
+    @Mapping(target = "cargos", ignore = true)  // cargo vem do Keycloak
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "ativo", ignore = true)
     @Mapping(target = "criado_em", ignore = true)
-    UsuarioModel toEntity(@Valid UsuarioRequestDTO dto);
+    UsuarioModel toEntity(UsuarioRequestDTO dto);
 
     UsuarioResponseDTO toResponse(UsuarioModel model);
 }

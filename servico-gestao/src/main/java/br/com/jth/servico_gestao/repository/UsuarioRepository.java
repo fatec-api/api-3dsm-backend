b9e2a44ab6d02fbf2cbd54b28fb75b1ecad5ee75
Lukitta013 <lucasinacio8000@gmail.com>
2026-05-25 11:45:26 -0300

    Chore(US11-03):Criando lá no repositório o modelo do usuário e fazendo os Joins.

--- servico-gestao/src/main/java/br/com/jth/servico_gestao/repository/UsuarioRepository.java
+++ servico-gestao/src/main/java/br/com/jth/servico_gestao/repository/UsuarioRepository.java
@@ -3,13 +3,17 @@ package br.com.jth.servico_gestao.repository;
 import br.com.jth.servico_gestao.enums.usuario.Cargo;
 import br.com.jth.servico_gestao.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
+    @Query("SELECT DISTINCT u FROM UsuarioModel u JOIN u.cargos c WHERE u.ativo = true AND c = :cargo")
+    List<UsuarioModel> findByAtivoTrueAndCargo(@Param("cargo") Cargo cargo);
 
-    List<UsuarioModel> findByAtivoTrueAndCargo(Cargo cargo);
     List<UsuarioModel> findByAtivoTrue();
+
     boolean existsByEmail(String email);
 }

9953cfb408490cf83ff03ddf18f5f82917c18957
Lukitta013 <lucasinacio8000@gmail.com>
2026-05-25 11:09:28 -0300

    Chore(US11-03): Removendo senha.

--- servico-gestao/src/main/java/br/com/jth/servico_gestao/dto/request/UsuarioRequestDTO.java
+++ servico-gestao/src/main/java/br/com/jth/servico_gestao/dto/request/UsuarioRequestDTO.java
@@ -1,10 +1,7 @@
 package br.com.jth.servico_gestao.dto.request;
 
-import br.com.jth.servico_gestao.enums.usuario.Cargo;
 import lombok.Data;
-
 import jakarta.validation.constraints.*;
-
 import java.math.BigDecimal;
 
 @Data
@@ -17,19 +14,10 @@ public class UsuarioRequestDTO {
     @Email(message = "E-mail informado é inválido.")
     private String email;
 
-    @NotBlank(message = "Preencha todos os campos obrigatórios (nome, e-mail, senha, confirme senha ou valor/hora).")
-    @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-={}|:<>?]).{8,}$",
-            message = "Senha inválida..."
-    )
-    private String senha;
-
     @NotNull(message = "Preencha todos os campos obrigatórios.")
     @DecimalMin(value = "0.01", message = "O valor/hora deve ser maior que zero.")
     private BigDecimal valorHora;
 
-    @NotNull(message = "Cargo é obrigatório.")
-    private Cargo cargo;
-
+    // Cargo removido — é atribuído pelo Keycloak via RabbitMQ após criação
     private String nivelExperiencia;
 }
